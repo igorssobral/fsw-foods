80395cee57c3c753f3d590cdb244bba0505d1374
Igor Sobral <113361588+igorssobral@users.noreply.github.com>
2024-05-02 08:56:18 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const Home = () => {
 
       <div className="px-5 pt-6">
         <Image
-          src="/promo-banner.png"
+          src="/promo-banner-01.png"
           alt="Até 30% de desconto em pizzas"
           height={0}
           width={0}
