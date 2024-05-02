3f6253dc4e8cd72778d55a17b3d3a99716c818fa
Igor Sobral <113361588+igorssobral@users.noreply.github.com>
2024-05-02 08:55:33 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./components/category-list";
 import Header from "./components/header";
 import Search from "./components/search";
@@ -9,7 +10,21 @@ const Home = () => {
       <div className="px-5 pt-6">
         <Search />
       </div>
-      <CategoryList />
+      <div className="px-5 pt-6">
+        <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner.png"
+          alt="Até 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          className="h-auto w-full"
+          sizes="100vw"
+          quality={100}
+        />
+      </div>
     </>
   );
 };
