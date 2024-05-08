4b67cdc599d4944f3220e376d56706b1aed08e07
Igor Sobral <113361588+igorssobral@users.noreply.github.com>
2024-05-08 18:21:06 -0300

    feat: add recommended restaurants page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { ChevronRightIcon } from "lucide-react";
 import { db } from "./_lib/prisma";
 import PromoBanner from "./components/promo-banner";
 import RestaurantList from "./components/restaurant-list";
+import Link from "next/link";
 
 const Home = async () => {
   const products = await db.product.findMany({
@@ -63,12 +64,14 @@ const Home = async () => {
       <div className="space-y-4 py-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Restaurantes Recomendados</h2>
-          <Button
-            variant="ghost"
-            className="p-0 h-fit text-primary hover:bg-transparent"
-          >
-            Ver todos <ChevronRightIcon size={16} />
-          </Button>
+          <Link href="/restaurants/recommended">
+            <Button
+              variant="ghost"
+              className="p-0 h-fit text-primary hover:bg-transparent"
+            >
+              Ver todos <ChevronRightIcon size={16} />
+            </Button>
+          </Link>
         </div>
         <RestaurantList />
       </div>
---
+++ app/restaurants/recommended/page.tsx
@@ -0,0 +1,27 @@
+import { db } from "@/app/_lib/prisma";
+import Header from "@/app/components/header";
+import RestaurantItem from "@/app/components/restauran-item";
+
+const RecommendedRestaurants = async () => {
+  const restaurants = await db.restaurant.findMany({});
+
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h2 className="text-lg font-semibold">Restaurantes Favoritos</h2>
+        <div className="w-full flex flex-col gap-6">
+          {restaurants.map((restaurant) => (
+            <RestaurantItem
+              key={restaurant.id}
+              restaurant={restaurant}
+              className="min-w-full max-w-full"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RecommendedRestaurants;
