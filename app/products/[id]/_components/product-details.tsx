8d1d26269974330c500975d461fd3203848d0b80
Igor Sobral <113361588+igorssobral@users.noreply.github.com>
2024-05-08 14:18:34 -0300

    fix: edit  impots

---
+++ app/products/[id]/_components/product-details.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import {
+  calculateProductTotalPrice,
+  formatCurrency,
+} from "@/app/_helpers/price";
+import DeliveryInfo from "@/app/components/delivery-info";
+import DiscountBadge from "@/app/components/discount-badge";
+import ProductList from "@/app/components/product-list";
+import { Button } from "@/app/components/ui/button";
+import { Prisma } from "@prisma/client";
+import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+
+interface ProductDetailProps {
+  product: Prisma.ProductGetPayload<{
+    include: {
+      restaurant: true;
+    };
+  }>;
+  complementaryProducts: Prisma.ProductGetPayload<{
+    include: {
+      restaurant: true;
+    };
+  }>[];
+}
+
+const ProductDetails = ({
+  product,
+  complementaryProducts,
+}: ProductDetailProps) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleIncreaseQuantityClick = () =>
+    setQuantity((currentState) => currentState + 1);
+  const handleDecreaseQuantityClick = () =>
+    setQuantity((currentState) => {
+      if (currentState === 1) return 1;
+
+      return currentState - 1;
+    });
+
+  return (
+    <div className="py-5 z-50 mt-[-1.5rem] relative bg-white rounded-tl-3xl rounded-tr-3xl">
+      <div className="flex items-center gap-[0.375rem] px-5">
+        <div className="relative h-6 w-6">
+          <Image
+            src={product.restaurant.imageUrl}
+            alt={product.restaurant.name}
+            fill
+            className="rounded-full object-cover"
+          />
+        </div>
+        <span className="textxs text-muted-foreground">
+          {product.restaurant.name}
+        </span>
+      </div>
+      <h1 className="mb-2 mt-1 text-xl font-semibold px-5">{product.name}</h1>
+      <div className="flex justify-between px-5">
+        <div>
+          <div className="flex items-center gap-2">
+            <h2 className="text-xl font-semibold">
+              {formatCurrency(calculateProductTotalPrice(product))}
+            </h2>
+            {product.discountPercentage > 0 && (
+              <DiscountBadge product={product} />
+            )}
+          </div>
+          {product.discountPercentage > 0 && (
+            <p className="text-muted-foreground text-sm">
+              De: {formatCurrency(Number(product.price))}
+            </p>
+          )}
+        </div>
+        <div className="flex gap-2 items-center text-center">
+          <Button
+            size="icon"
+            variant="ghost"
+            className="border border-solid border-muted-foreground"
+            onClick={handleDecreaseQuantityClick}
+          >
+            <ChevronLeftIcon />
+          </Button>
+          <span className="w-4">{quantity}</span>
+          <Button size="icon" onClick={handleIncreaseQuantityClick}>
+            <ChevronRightIcon />
+          </Button>
+        </div>
+      </div>
+
+      <div className="px-5">
+        <DeliveryInfo restaurant={product.restaurant} />
+      </div>
+
+      <div className="mt-6 space-y-3 px-5">
+        <h3 className="font-semibold">Sobre</h3>
+        <p className="text-sm text-muted-foreground">{product.description}</p>
+      </div>
+
+      <div className="mt-6 space-y-3">
+        <h3 className="font-semibold px-5">Sucos</h3>
+        <ProductList products={complementaryProducts} />
+      </div>
+
+      <div className="mt-6 px-5">
+        <Button className="w-full font-semibold">Adicionar a sacola</Button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
