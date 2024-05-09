481f1a7cf401bac403daaff794093fa454624a62
Igor Sobral <113361588+igorssobral@users.noreply.github.com>
2024-05-08 22:30:54 -0300

    feat: add recommended products page

--- app/page.tsx
+++ app/page.tsx
@@ -45,12 +45,14 @@ const Home = async () => {
       <div className="space-y-4 py-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Pedidos Recomendados</h2>
-          <Button
-            variant="ghost"
-            className="p-0 h-fit text-primary hover:bg-transparent"
-          >
-            Ver todos <ChevronRightIcon size={16} />
-          </Button>
+          <Link href="/products/recommended">
+            <Button
+              variant="ghost"
+              className="p-0 h-fit text-primary hover:bg-transparent"
+            >
+              Ver todos <ChevronRightIcon size={16} />
+            </Button>
+          </Link>
         </div>
         <ProductList products={products} />
       </div>
---
+++ app/products/recommended/page.tsx
@@ -0,0 +1,40 @@
+import { db } from "@/app/_lib/prisma";
+import Header from "@/app/components/header";
+import ProductItem from "@/app/components/product-item";
+
+const RecommendedProductsPage = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 20,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h2 className="text-lg mb-6 font-semibold">Pedidos Recomendados</h2>
+        <div className="grid grid-cols-2 gap-6">
+          {products.map((product) => (
+            <ProductItem
+              key={product.id}
+              product={product}
+              className="min-w-full"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RecommendedProductsPage;
