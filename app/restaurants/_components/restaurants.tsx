fc58dafb38b021878d1c5f357750bdce2ed489a1
Igor Sobral <113361588+igorssobral@users.noreply.github.com>
2024-06-07 09:59:19 -0300

    fix: restaurants page

--- app/restaurants/_components/restaurants.tsx
+++ app/restaurants/_components/restaurants.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import { Restaurant } from "@prisma/client";
+import { Restaurant, UserFavoriteRestaurant } from "@prisma/client";
 import { notFound, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { searchForRestaurants } from "../_actions/search";
 import RestaurantItem from "@/app/_components/restauran-item";
 import Header from "@/app/_components/header";
 
-const Restaurants = () => {
+interface RestaurantProps {
+  userFavoriteRestaurants: UserFavoriteRestaurant[];
+}
+
+const Restaurants = ({ userFavoriteRestaurants }: RestaurantProps) => {
   const searchParams = useSearchParams();
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
   const searchFor = searchParams.get("search");
@@ -27,13 +31,16 @@ const Restaurants = () => {
     <>
       <Header />
       <div className="px-5 py-6">
-        <h2 className="text-lg mb-6 font-semibold">Restaurantes Favoritos</h2>
+        <h2 className="text-lg mb-6 font-semibold">
+          Restaurantes Recomendados
+        </h2>
         <div className="w-full flex flex-col gap-6">
           {restaurants.map((restaurant) => (
             <RestaurantItem
               key={restaurant.id}
               restaurant={restaurant}
               className="min-w-full max-w-full"
+              userFavoriteRestaurant={userFavoriteRestaurants}
             />
           ))}
         </div>
--- app/restaurants/recommended/page.tsx
+++ app/restaurants/recommended/page.tsx
@@ -1,21 +1,34 @@
 import { db } from "@/app/_lib/prisma";
 import Header from "@/app/_components/header";
 import RestaurantItem from "@/app/_components/restauran-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/_lib/auth";
 
 const RecommendedRestaurants = async () => {
   const restaurants = await db.restaurant.findMany({});
-
+  const session = await getServerSession(authOptions);
+  const userFavoriteRestaurants = await db.userFavoriteRestaurant.findMany({
+    where: {
+      userId: session?.user.id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
   return (
     <>
       <Header />
       <div className="px-5 py-6">
-        <h2 className="text-lg mb-6 font-semibold">Restaurantes Favoritos</h2>
+        <h2 className="text-lg mb-6 font-semibold">
+          Restaurantes Recomendados
+        </h2>
         <div className="w-full flex flex-col gap-6">
           {restaurants.map((restaurant) => (
             <RestaurantItem
               key={restaurant.id}
               restaurant={restaurant}
               className="min-w-full max-w-full"
+              userFavoriteRestaurant={userFavoriteRestaurants}
             />
           ))}
         </div>
